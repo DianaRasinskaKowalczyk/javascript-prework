9bed9c78af0ad2774912820df0fe351d53047e39
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2022-03-17 11:42:24 +0100

    Change computerMove var to const

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 printMessage('Wylosowana liczba to: ' + randomNumber);
 
 
-var computerMove, playerInput;
+var playerInput;
 
 /**
  * Describe this function...
@@ -55,7 +55,7 @@ function displayResult(argPlayerMove, argComputerMove) {
 const playerMove = argButtonName;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
-computerMove = getMoveName(randomNumber);
+const computerMove = getMoveName(randomNumber);
 console.log('ruch komputera to: ' + computerMove);
 displayResult(playerMove, computerMove);
 }
