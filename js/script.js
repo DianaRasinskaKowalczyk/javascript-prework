211fc6a89e7e1320e50312a373ab4fceb1bef818
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2022-03-17 11:47:09 +0100

    Delete playerInput

--- js/script.js
+++ js/script.js
@@ -12,10 +12,9 @@ function buttonClicked(argButtonName) {
 
 
 let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
 printMessage('Wylosowana liczba to: ' + randomNumber);
 
-
-var playerInput;
 
 /**
  * Describe this function...
@@ -53,8 +52,6 @@ function displayResult(argPlayerMove, argComputerMove) {
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
 const playerMove = argButtonName;
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
 const computerMove = getMoveName(randomNumber);
 console.log('ruch komputera to: ' + computerMove);
 displayResult(playerMove, computerMove);
